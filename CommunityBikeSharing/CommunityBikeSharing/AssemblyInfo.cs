10855ee2a5c61013405e72e2d27a62ea6d20673a
David <git@davidmannes.de>
2021-05-14 16:02:20 +0200

    Add icons

--- CommunityBikeSharing/CommunityBikeSharing/AssemblyInfo.cs
+++ CommunityBikeSharing/CommunityBikeSharing/AssemblyInfo.cs
@@ -1,3 +1,7 @@
+using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
 [assembly: XamlCompilation(XamlCompilationOptions.Compile)]
+
+[assembly: ExportFont("Font Awesome 5 Free-Regular-400.otf", Alias = "RegularIcons")]
+[assembly: ExportFont("Font Awesome 5 Free-Solid-900.otf", Alias = "SolidIcons")]
