ba310864eb32e9704ee0a8e75535ac2efd09addb
David <git@davidmannes.de>
2021-05-16 12:30:53 +0200

    Add searchfield to MapModal

--- CommunityBikeSharing/CommunityBikeSharing/Views/MapModalPage.xaml.cs
+++ CommunityBikeSharing/CommunityBikeSharing/Views/MapModalPage.xaml.cs
@@ -1,4 +1,6 @@
 ﻿#nullable enable
+using System;
+using System.Linq;
 using CommunityBikeSharing.Services;
 using CommunityBikeSharing.ViewModels;
 using Microsoft.Extensions.DependencyInjection;
@@ -27,10 +29,28 @@ namespace CommunityBikeSharing.Views
 			}
 		}
 
+		// The binding for Pin.Position is not working, so we have to set it in code-behind
 		private void OnMapClicked(object sender, MapClickedEventArgs e)
 		{
 			((MapModalViewModel)BindingContext).SelectedLocation = e.Position.ToLocation();
 			Pin.Position = e.Position;
+		}
+
+		private async void OnSearchButtonClicked(object sender, EventArgs e)
+		{
+			var searchText = SearchField.Text;
+
+			if (string.IsNullOrEmpty(searchText))
+			{
+				return;
+			}
+
+			var positions = await Geocoder.GetPositionsForAddressAsyncFunc(searchText);
+			var position = positions.FirstOrDefault();
+
+			((MapModalViewModel)BindingContext).SelectedLocation = position.ToLocation();
+			Pin.Position = position;
+			Map.MoveToRegion(MapSpan.FromCenterAndRadius(position, Distance.FromKilometers(1)));
 		}
 	}
 }
