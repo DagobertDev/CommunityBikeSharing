10253f16cd2ff45e2767ef637a380fe128e5ac80
David <git@davidmannes.de>
2021-06-04 16:30:40 +0200

    Show icons next to bikes/stations/users

---
+++ CommunityBikeSharing/CommunityBikeSharing/Assets/Fonts.cs
@@ -0,0 +1,14 @@
+﻿using CommunityBikeSharing.Assets;
+using Xamarin.Forms;
+
+[assembly: ExportFont("Font Awesome 5 Free-Regular-400.otf", Alias = Fonts.RegularIcons)]
+[assembly: ExportFont("Font Awesome 5 Free-Solid-900.otf", Alias = Fonts.SolidIcons)]
+
+namespace CommunityBikeSharing.Assets
+{
+	public static class Fonts
+	{
+		public const string RegularIcons = nameof(RegularIcons);
+		public const string SolidIcons = nameof(SolidIcons);
+	}
+}
