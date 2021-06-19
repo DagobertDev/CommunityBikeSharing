88434f87dcdc11feb52ec06d7114b19d42f842e6
David <git@davidmannes.de>
2021-06-19 19:23:57 +0200

    Bugfix: Calling DialogService from tasks

--- CommunityBikeSharing/CommunityBikeSharing/Services/DialogService.cs
+++ CommunityBikeSharing/CommunityBikeSharing/Services/DialogService.cs
@@ -3,6 +3,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Xamarin.Essentials;
 using Xamarin.Forms;
 
 namespace CommunityBikeSharing.Services
@@ -10,13 +11,15 @@ namespace CommunityBikeSharing.Services
 	public class DialogService : IDialogService
 	{
 		public Task ShowMessage(string title, string message, string buttonText)
-			=> Application.Current.MainPage.DisplayAlert(title, message, buttonText);
+			=> MainThread.InvokeOnMainThreadAsync(()
+				=> Application.Current.MainPage.DisplayAlert(title, message, buttonText));
 
 		public Task ShowError(string title, string message, string buttonText)
 			=> ShowMessage(title, message, buttonText);
 
 		public Task<bool> ShowConfirmation(string title, string message, string confirm, string cancel)
-			=> Application.Current.MainPage.DisplayAlert(title, message, confirm, cancel);
+			=> MainThread.InvokeOnMainThreadAsync(()
+				=> Application.Current.MainPage.DisplayAlert(title, message, confirm, cancel));
 
 		public Task<string> ShowTextEditor(string title, string message, string confirm, string cancel,
 			IDialogService.KeyboardType keyboardType)
@@ -29,7 +32,8 @@ namespace CommunityBikeSharing.Services
 				_ => Keyboard.Default
 			};
 
-			return Application.Current.MainPage.DisplayPromptAsync(title, message, confirm, cancel, keyboard: keyboard);
+			return MainThread.InvokeOnMainThreadAsync(() =>
+				Application.Current.MainPage.DisplayPromptAsync(title, message, confirm, cancel, keyboard: keyboard));
 		}
 
 		public async Task ShowActionSheet(string title, string cancel, IEnumerable<(string, Action)> actionsEnumerable)
@@ -41,8 +45,9 @@ namespace CommunityBikeSharing.Services
 				return;
 			}
 
-			var result = await Application.Current.MainPage.DisplayActionSheet(title, cancel, null,
-				actions.Select(a => a.Item1).ToArray());
+			var result = await MainThread.InvokeOnMainThreadAsync(() =>
+				Application.Current.MainPage.DisplayActionSheet(title, cancel, null,
+					actions.Select(a => a.Item1).ToArray()));
 
 			actions.SingleOrDefault(action => action.Item1 == result).Item2?.Invoke();
 		}
