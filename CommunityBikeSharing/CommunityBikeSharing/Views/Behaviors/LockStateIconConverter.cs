0660ae550e2e78763343814c87df3b56ef74b5ab
David <git@davidmannes.de>
2021-06-22 10:25:37 +0200

    Add lock state indicator to overview

---
+++ CommunityBikeSharing/CommunityBikeSharing/Views/Behaviors/LockStateIconConverter.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Globalization;
+using CommunityBikeSharing.Models;
+using FontAwesome;
+using Xamarin.Forms;
+
+namespace CommunityBikeSharing.Views.Behaviors
+{
+	public class LockStateIconConverter : IValueConverter
+	{
+		public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+		{
+			return (Lock.State)value switch
+			{
+				Lock.State.None => string.Empty,
+				Lock.State.Unknown => string.Empty,
+				Lock.State.Open => FontAwesomeIcons.LockOpen,
+				Lock.State.Closed => FontAwesomeIcons.Lock,
+				_ => throw new ArgumentOutOfRangeException(nameof(value), value, null)
+			};
+		}
+
+		public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture) => throw new NotImplementedException();
+	}
+}
