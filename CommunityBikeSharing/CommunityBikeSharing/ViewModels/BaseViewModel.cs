3a8521e9d0eb92db678f45083024db5b568c8d73
David <git@davidmannes.de>
2021-07-23 10:06:29 +0200

    Make CreateCommand protected

--- CommunityBikeSharing/CommunityBikeSharing/ViewModels/BaseViewModel.cs
+++ CommunityBikeSharing/CommunityBikeSharing/ViewModels/BaseViewModel.cs
@@ -9,15 +9,15 @@ namespace CommunityBikeSharing.ViewModels
 	{
 		public virtual Task InitializeAsync() => Task.CompletedTask;
 
-		public ICommand CreateCommand(Action action) => CommandFactory.Create(action);
-		public ICommand CreateCommand(Action action, Func<bool> canExecute) => CommandFactory.Create(action, canExecute);
-		public ICommand CreateCommand<T>(Action<T> action) => CommandFactory.Create(action);
-		public ICommand CreateCommand<T>(Action<T> action, Func<T, bool> canExecute) => CommandFactory.Create(action, canExecute);
-		public ICommand CreateCommand(Func<Task> task) => CommandFactory.Create(task);
-		public ICommand CreateCommand(Func<Task> task, Func<bool> canExecute) 
+		protected ICommand CreateCommand(Action action) => CommandFactory.Create(action);
+		protected ICommand CreateCommand(Action action, Func<bool> canExecute) => CommandFactory.Create(action, canExecute);
+		protected ICommand CreateCommand<T>(Action<T> action) => CommandFactory.Create(action);
+		protected ICommand CreateCommand<T>(Action<T> action, Func<T, bool> canExecute) => CommandFactory.Create(action, canExecute);
+		protected ICommand CreateCommand(Func<Task> task) => CommandFactory.Create(task);
+		protected ICommand CreateCommand(Func<Task> task, Func<bool> canExecute) 
 			=> CommandFactory.Create(task, canExecute);
-		public ICommand CreateCommand<T>(Func<T, Task> task) => CommandFactory.Create(task!);
-		public ICommand CreateCommand<T>(Func<T, Task> task, Func<T, bool> canExecute) 
+		protected ICommand CreateCommand<T>(Func<T, Task> task) => CommandFactory.Create(task!);
+		protected ICommand CreateCommand<T>(Func<T, Task> task, Func<T, bool> canExecute) 
 			=> CommandFactory.Create(task!, o => o is T t && canExecute(t));
 	}
 }
